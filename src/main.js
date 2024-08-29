5dd8b7dfb996d866c501b6dccbe56d4f9e0b7d78
AzharJacobs <azharj2003@gmail.com>
2024-08-29 16:03:54 +0200

    Structure

--- src/main.js
+++ src/main.js
@@ -3,5 +3,12 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import './assets/style.css';
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Import Bootstrap JS (Optional)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
 
 createApp(App).use(store).use(router).mount('#app')
