cd082f63fe83744601b828e2c808f261adb2397e
AzharJacobs <azharj2003@gmail.com>
2024-09-12 16:11:34 +0200

    heheh

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ import { productsRouter } from './route/productsRouter.js'
 let port = process.env.PORT || 4004
 const app = express()
 
-app.use(cors())
+const corsOptions = {
+    origin: ['http://localhost:8080', 'https://capstone-eomp-yhlw.onrender.com'],
+    credentials: true
+  };
+
+  app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.static('public'))
  
